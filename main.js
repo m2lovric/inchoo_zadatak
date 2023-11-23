64401e934d9f1306340ca0d766e638cd8ee1154a
m2lovric <m2lovric@gmail.com>
2023-11-23 21:52:06 +0100

    Add shipping, style cart and add collapsible style

--- main.js
+++ main.js
@@ -9,6 +9,13 @@ const items = [
   { id: 'second', img: 'HQ6351_1', name: 'NMD_R1', quantity: 1, price: 150 },
 ];
 
+const shipping = 5;
+
+let USDollar = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+});
+
 function startPage() {
   localStorage.setItem('state', JSON.stringify(items));
   allItems();
@@ -23,11 +30,15 @@ function allItems() {
     const section = document.createElement('section');
     section.innerHTML = `
     <article class="flex items-center justify-between">
-      <img src="./public/${item.img}.webp" class=" w-64 h-auto justify-start"/>
-      <h1 class="justify-start">${item.name}</h1>
-      <input type="number"  value=${item.quantity} id=${item.id} class="justify-end"/>
-      <p class="justify-end">${item.price}$</p>
-      <button class="justify-end"><img src="delete.svg" alt="delete"/></button>
+      <img src="./public/${item.img}.webp" class=" w-64 h-auto flex-1"/>
+      <h1 class="flex-1 font-bebas-neue text-xl">${item.name}</h1>
+      <input type="number"  value=${item.quantity} id=${
+      item.id
+    } class="w-12 h-12 pl-4 border-2 text-center border-black"/>
+      <p class="font-titillium-web font-bold w-1/6 ml-12">${USDollar.format(
+        item.price
+      )}</p>
+      <button class="ml-auto justify-self-end w-1/12"><img src="delete.svg" alt="delete"/></button>
     </article>
     `;
     cart.appendChild(section);
@@ -40,8 +51,13 @@ function totalAmount() {
   state.map((item) => {
     total += item.quantity * item.price;
   });
-  document.querySelector('#total').innerHTML = `${total}`;
-  document.querySelector('#subtotal').innerHTML = `${total}`;
+  document.querySelector('#total').innerHTML = `${USDollar.format(
+    total + shipping
+  )}`;
+  document.querySelector('#subtotal').innerHTML = `${USDollar.format(total)}`;
+  document.querySelector('#shipping').innerHTML = `${USDollar.format(
+    shipping
+  )}`;
   console.log(total);
 }
 
@@ -63,13 +79,23 @@ document.getElementById('second').addEventListener('change', (e) => {
   changeState(e);
 });
 
-document.getElementById('toggle').addEventListener('click', (e) => {
+const collapsible = document.querySelector('#collapsible');
+const toggle = document.getElementById('toggle');
+toggle.innerHTML = `<img src="plus.svg" alt="plus">`;
+
+toggle.addEventListener('click', (e) => {
   const content = document.getElementById('content');
   if (content.classList.contains('hidden')) {
+    toggle.innerHTML = `<img src="cancel.svg" alt="cancel">`;
     content.classList.add('active');
+    collapsible.classList.add('text-green-700');
+    collapsible.classList.remove('text-black');
     content.classList.remove('hidden');
   } else {
+    toggle.innerHTML = `<img src="plus.svg" alt="plus">`;
     content.classList.add('hidden');
     content.classList.remove('active');
+    collapsible.classList.add('text-black');
+    collapsible.classList.remove('text-green-700');
   }
 });
